ec0c5812f13ed4ada5ece5b9e1b7061aa0280d9e
TayfurSafakGencay <safak.gencay@gmail.com>
2025-12-02 22:30:38 +0300

    scripts

---
+++ BlogApp/ViewComponents/NewPosts.cs
@@ -0,0 +1,22 @@
+﻿using BlogApp.Data.Abstract;
+using BlogApp.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.ViewComponents;
+
+public class NewPosts : ViewComponent
+{
+	private IPostRepository _postRepository;
+
+	public NewPosts(IPostRepository postRepository)
+	{
+		_postRepository = postRepository;
+	}
+
+	public IViewComponentResult Invoke()
+	{
+		List<Post> posts = _postRepository.Posts.OrderByDescending(p => p.PublishedOn).Take(5).ToList();
+		
+		return View(posts);
+	}
+}
